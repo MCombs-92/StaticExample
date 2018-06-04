7723b664108c7e7240b63065aa10e455d87280c1
Mitchell Combs <mitch.combs.92@gmail.com>
2018-06-04 13:37:43 -0400

    Initial Commit

--- StaticExample/Program.cs
+++ StaticExample/Program.cs
@@ -5,8 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace StaticExample {
+
+
 	class Program {
 		static void Main(string[] args) {
+
+			Technology Git = new Technology();
+			Git.Name = "Git/GitHub";
+			Git.Difficulty = Technology.DifficultyType.Medium;
+
+			Technology SQL = new Technology();
+			SQL.Name = "SQL Server";
+			SQL.Difficulty = Technology.DifficultyType.Hard;
+
+			Technology CSharp = new Technology("C Sharp Language");
+
+			Technology EF = new Technology("EntityFramework", Technology.DifficultyType.Easiest);
 		}
 	}
 }
---
+++ StaticExample/Technology.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExample {
+	class Technology {
+
+		public enum DifficultyType {
+			Hardest = 1, Hard, Medium, Easy, Easiest
+		};
+
+		public string Name { get; set; }
+		public DifficultyType Difficulty{ get; set; }
+		public int Id { get; set; }
+
+		public static int Counter { get; set; } = 0;
+
+		private void Init() {
+			Id = ++Counter;
+			Difficulty = DifficultyType.Medium;
+		}
+
+		// Constructor
+		public Technology() {
+			Init();
+
+		
+		}
+
+		//Cunstructor
+		public Technology(string name) {
+			Init();
+			Name = name;
+			
+		}
+
+
+		// Constructor
+		public Technology(string name, DifficultyType difficulty) {
+			Init();
+			Name = name;
+			Difficulty = difficulty;
+		}
+	}
+}
